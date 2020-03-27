fd7e7a27ed7297bad0d942eb38c18bbe0f97b6e1
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-03-27 22:05:34 +0100

    Dodanie endointu korzystającego z widoku jsp

--- src/main/java/academy/learnprogramming/springbootdemo/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/springbootdemo/controller/DemoController.java
@@ -2,6 +2,7 @@ package academy.learnprogramming.springbootdemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,5 +17,16 @@ public class DemoController {
         log.info("demo method called");
 
         return "Hello Spring Boot!";
+    }
+
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+
+        log.info("welcome method called");
+
+        model.addAttribute("message", "Welcome to Spring Boot");
+
+        return "welcome"; //endpoint wykona się, lecz widok nie wyświetli się w przeglądarce, co jest związane z wbudowanym w Intellij-a tomcatem
+                          //a nie z naszym błędem. Błąd tomcata dotyczy jakiegoś zahardkodowanego adresu "/error", który w jakiś sposób kłóci się z jsp
     }
 }
